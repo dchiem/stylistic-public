878ee9b62a351b0ac6a4ab953fc22828e7e785a0
Jamie Irvine <jirvine@cs.stanford.edu>
2014-02-11 19:28:00 -0800

    Added small images for browse page and made more duplicate images in data. Also browse now loads images dynamically in loop

--- routes/browse.js
+++ routes/browse.js
@@ -2,5 +2,24 @@
 var data = require('../data.json');
 
 exports.view = function(req, res){
-	res.render('browse', data);
+    data['helpers'] =
+        {
+            addRow: function(index, options) {
+                        if ((index)%3 == 0) {
+                            return options.fn(this);
+                        }
+                        else {
+                            return options.inverse(this);
+                        }
+                    },
+            endRow: function(index, options) {
+                        if ((index)%3 == 2) {
+                            return options.fn(this);
+                        }
+                        else {
+                            return options.inverse(this);
+                        }
+                    }
+        };
+    res.render('browse', data);
 };
