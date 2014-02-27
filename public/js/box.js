ec3f52da0606598d644cb2fc6e347424a544c926
Jamie Irvine <jirvine@cs.stanford.edu>
2014-02-26 19:25:06 -0800

    Add items in boxes to your box

--- public/js/box.js
+++ public/js/box.js
@@ -1,12 +1,14 @@
+var thisBoxId;
+
 $(document).ready(function() {
     initializePage();
 });
 
 function initializePage() {
     console.log("Javascript connected!");
+    thisBoxId = $(".star").attr("id");
     $(".star").click(function(){
-        var id = $(".star").attr("id");
-        var likeData = { 'id': id};
+        var likeData = { 'id': thisBoxId};
         if ($(this).attr("class") == "star") {
             this.src = this.src.replace("greystar.png","yellowstar.png");
             $.ajax({
@@ -31,18 +33,43 @@ function initializePage() {
     });
 
     $(".dropdown").hide();
-    $(".add").click(function(event) {
-        $(this).find('img').toggle();
-        var itemID = event.target.id;
+    $("body").click(function(event) {
+        $(".add").find(".dropdown").fadeOut(200);
+    });
 
-        console.log("This is the item ID: " + itemID);
-        $(".dropdown").toggle(200);
+    $(".mybox").click(function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        $(this).toggleClass("green");
+        $(this).parent().parent().parent().parent().find('img').toggle();
+        var boxId = $(this).attr("id");
+        var itemId = $(this).parent().parent().parent().attr("id")
+        console.log("boxid : " + boxId + " itemId : " + itemId);
+        if ($(this).hasClass("green")) {
+            addToBox(boxId, itemId);
+        } else {
+            removeFromBox(boxId, itemId);
+        }
+    });
 
+    $(".add img").click(function(event) {
+        event.stopPropagation();
+        $(this).parent().find(".dropdown").toggle(200);
     });
 }
 
-/*
-   function test(err, users) {
-   console.log("User = " +  users[0]);
-   }
-   */
+function addToBox(boxId, itemId) {
+    $.ajax({
+        url: '/addToBox',
+        type: 'get',
+        data: { boxId : thisBoxId, newBoxId : boxId, itemId : itemId}
+    });
+}
+
+function removeFromBox(boxId, itemId) {
+    $.ajax({
+        url: '/removeFromBox',
+        type: 'get',
+        data: { boxId : thisBoxId, newBoxId : boxId, itemId : itemId}
+    });
+}
--- routes/box.js
+++ routes/box.js
@@ -2,6 +2,7 @@ var models = require('../models');
 
 exports.view = function(req, res){
     var sessionUser;
+    var myboxes;
     var isLiked;
     var id = req.query.id;
     if (req.user) {
@@ -10,14 +11,26 @@ exports.view = function(req, res){
         if (contains(req.user.likes, id)) {
             isLiked = true;
         }
+        models.Boxes
+            .find({"user": sessionUser})
+            .exec(getBox);
+    } else {
+        getBox("","");
     }
-    
-    models.Boxes
-        .find({_id: id})
-        .exec(renderBox);
+
+    function getBox(err, boxes) {
+        if (err) console.log(err);
+        myboxes = boxes;
+        console.log(boxes);
+        models.Boxes
+            .find({_id: id})
+            .exec(renderBox);
+    }
 
     function renderBox(err, boxes) {
-        res.render('box', {"thebox":boxes[0], "sessionUser":sessionUser, "isLiked":isLiked});
+        if (err) console.log(err);
+        console.log("myboxes : " + myboxes);
+        res.render('box', {"myboxes": myboxes, "thebox":boxes[0], "sessionUser":sessionUser, "isLiked":isLiked});
     }
 
     function contains(array, element) {
@@ -28,3 +41,52 @@ exports.view = function(req, res){
         }
     }
 };
+
+exports.addToBox = function(req, res) {
+    var boxId = req.query.boxId;
+    var newBoxId = req.query.newBoxId;
+    var itemId = req.query.itemId;
+    console.log("boxid : " + boxId + " newBoxId : " + newBoxId + " itemId : " + itemId);
+    models.Boxes
+        .find({_id : boxId}, {boxitems : {$elemMatch: {_id : itemId}}})
+        .exec(addItemToBox);
+
+    function addItemToBox(err, items) {
+        var item = items[0].boxitems[0];
+        console.log("item : " + item);
+        models.Boxes
+            .update({_id : newBoxId}, {$push: {boxitems :
+                                {   "name": item.name, 
+                                    "imageURL": item.imageURL, 
+                                    "link" : item.link, 
+                                    _id : item._id}}})
+            .exec(doNothing);
+    }
+
+    function doNothing(err) {
+        if (err) console.log(err);
+    }
+}
+
+exports.removeFromBox = function(req, res) {
+    var boxId = req.query.boxId;
+    var newBoxId = req.query.newBoxId;
+    var itemId = req.query.itemId;
+    console.log("boxid : " + boxId + " newBoxId : " + newBoxId + " itemId : " + itemId);
+    models.Boxes
+        .find({_id : boxId}, {boxitems : {$elemMatch: {_id : itemId}}})
+        .exec(removeItemFromBox);
+
+    function removeItemFromBox(err, items) {
+        var item = items[0].boxitems[0];
+        console.log("item : " + item);
+        models.Boxes
+            .update({_id : newBoxId}, 
+                    {$pull: {boxitems : { _id : item._id}}})
+            .exec(doNothing);
+    }
+
+    function doNothing(err) {
+        if (err) console.log(err);
+    }
+}
