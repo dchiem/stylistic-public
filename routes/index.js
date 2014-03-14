14b148fc3da19f132ef4d767fc00bb4d0a039874
Jamie Irvine <jirvine@cs.stanford.edu>
2014-03-14 13:44:02 -0700

    fix gender index issue

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ exports.view = function(req, res){
         res.redirect('home');
         return;
     }
-    req.logout();
     req.session.male = "";
     req.session.female = "";
-    res.redirect('home');
 	res.render('index');
 };
